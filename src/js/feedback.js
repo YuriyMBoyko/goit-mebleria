9f708361e526f81645642663d0731f97fd00afeb
Yuriy Boyko <YuriyMBoyko@gmail.com>
2026-08-15 03:07:34 +0300

    Fix bug with icons for rating

--- src/js/feedback.js
+++ src/js/feedback.js
@@ -5,8 +5,9 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'css-star-rating/css/star-rating.css';
 import { setElementVisible } from './helper.js';
-import { buildStarRatingMarkup } from './feedback-star.js'
+/*import { buildStarRatingMarkup } from './feedback-star.js'*/
 import feedbackData, { feedbackEmptyData } from './feedback-data.js';
+import spriteUrl from '../img/star-rating.icons.svg';
 
 document.addEventListener('DOMContentLoaded', () => {
   markupFeedbackData('.feedback-data-container', feedbackData.feedbacks);
@@ -138,3 +139,48 @@ function rateNormalize(value) {
   };
 }
 */
+
+
+
+
+
+export function buildStarRatingMarkup(selectorOrElement, rate) {
+
+  const { rateIntValue, rateIsHalf } = rateNormalize(rate);
+  const halfClass = rateIsHalf ? 'half' : '';
+
+  const starMarkup = getStarMarkup();
+/*  const starMarkup = getStarMarkupByTemplate(selectorOrElement);*/
+
+  const starsMarkup = Array.from({ length: 5 }, () => { return starMarkup; }).join('');
+
+  return `
+    <div class="rating star-svg value-${rateIntValue} ${halfClass} color-default feedback-star-wrapper">
+      <div class="star-container">${starsMarkup}</div>
+    </div>`;
+}
+
+export function buildStarRatingEmptyMarkup(rate) {
+  return '';
+}
+
+function getStarMarkup() {
+/*  const spriteUrl = new URL('../img/star-rating.icons.svg', import.meta.url).href;*/
+
+  return `
+    <div class="star">
+      <svg class="star-empty"><use href="${spriteUrl}#star-empty"></use></svg>
+      <svg class="star-half"><use href="${spriteUrl}#star-half"></use></svg>
+      <svg class="star-filled"><use href="${spriteUrl}#star-filled"></use></svg>
+    </div>`;
+}
+
+function rateNormalize(value) {
+  let rate = Math.round(10 * Math.max(0, (Math.min(5, Number(value) || 0))));
+  rate = (rate >= 33 && rate <= 37) ? 35 : ((rate >= 38 && rate <= 42) ? 40 : rate);
+  return {
+    rateValue: rate / 10,
+    rateIntValue: Math.floor(rate / 10),
+    rateIsHalf: (rate % 10) >= 5
+  };
+}
